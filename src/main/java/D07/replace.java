c5e5c8ce2bafb5161234387ab4b384bdf83ec3c8
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2023-09-05 00:00:06 +0200

    D0703

---
+++ src/main/java/D07/replace.java
@@ -0,0 +1,9 @@
+package D07;
+
+public class replace {
+    public static void main(String[] args) {
+
+        String str = "Java ogrenmek cok kolay";
+        System.out.println(str.replace("a","x"));
+    }
+}
