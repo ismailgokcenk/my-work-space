1b608e6520fab0b2a7d34384b789c37d2a5e5f78
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2023-09-05 01:32:48 +0200

    D0704

---
+++ src/main/java/D07/BSP.java
@@ -0,0 +1,22 @@
+package D07;
+
+public class BSP {
+    public static void main(String[] args) {
+        /*String str1 = "   Java ogrenmek123 Cok guzel@";
+        String str2 = str1.trim().replace("ogrenmek123","ogrenmek").replace("Cok","cok").replace("@",".");
+        System.out.println(str2);
+
+
+        System.out.println();
+        */
+
+
+
+        String str3 ="$13.99";
+        double doublestr3=Double.valueOf(str3);
+        System.out.println(doublestr3);
+
+
+
+    }
+}
