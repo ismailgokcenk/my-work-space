f0b86fb0accf1f5962c990237bbb134d461ee10f
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2023-09-06 01:39:07 +0200

    D0801

---
+++ src/main/java/D08/methodCreation.java
@@ -0,0 +1,17 @@
+package D08;
+
+public class methodCreation {
+
+    public static void main(String[] args) {
+
+         toplama(5,4);
+
+    }
+
+    public static void toplama (int a , int b){
+        System.out.println(a+b);
+    }
+    }
+
+
+
---
+++ src/main/java/D08/methodCreation2.java
@@ -0,0 +1,25 @@
+package D08;
+
+public class methodCreation2 {
+
+    public static void main(String[] args) {
+
+
+        int result = topla(1 ,2 );
+        System.out.println(result);
+
+        print("Ali Can");
+
+
+    }
+
+    public static int topla( int num1 , int num2){
+
+        return num1+num2;
+    }
+
+    public static void print(String str){
+        System.out.println(str);
+    }
+
+}
