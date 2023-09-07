83ebc370947aa128f2c8a0b3cdd5f8e5586b45b9
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2023-09-07 23:34:31 +0200

    D0901

---
+++ src/main/java/D09/forLoop2.java
@@ -0,0 +1,13 @@
+package D09;
+
+public class forLoop2 {
+    public static void main(String[] args) {
+
+
+        for (int i = 9; i <31  ; i++) {
+            System.out.print(i+",");
+
+        }
+
+    }
+}
---
+++ src/main/java/D09/forLoop3.java
@@ -0,0 +1,12 @@
+package D09;
+
+public class forLoop3 {
+    public static void main(String[] args) {
+
+        for (int i = 100; i >49 ; i--) {
+            System.out.print(i+",");
+
+        }
+
+    }
+}
