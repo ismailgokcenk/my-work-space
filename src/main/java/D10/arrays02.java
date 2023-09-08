1aa9ac0f22fa9d217a058cd407701660e142ea3d
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2023-09-08 16:04:49 +0200

    D1002

--- src/main/java/D10/arrays02.java
+++ src/main/java/D10/arrays02.java
@@ -8,6 +8,8 @@ public class arrays02 {
         int aint[]={9,10,11};
         System.out.println(Arrays.toString(aint));
 
+        System.out.println("aint.length = " + aint.length);
+
         int [] aint2 = new int[3];
         System.out.println(Arrays.toString(aint2));
         aint2[0]= 10;
--- src/main/java/D10/arrays3.java
+++ src/main/java/D10/arrays3.java
@@ -1,4 +1,16 @@
 package D10;
 
+import java.util.Arrays;
+
 public class arrays3 {
-}
+    public static void main(String[] args) {
+
+        int arrint[]={5,8,6,13,4};
+        Arrays.sort(arrint);
+        System.out.println(Arrays.toString(arrint));
+
+
+        }
+
+    }
+
