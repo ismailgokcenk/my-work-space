8feacfcab6c86959242cfad662d258fdc1e3e9b5
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2023-09-09 23:52:24 +0200

    d11

---
+++ src/main/java/D10/arrays7.java
@@ -0,0 +1,15 @@
+package D10;
+
+import java.util.Arrays;
+
+public class arrays7 {
+    public static void main(String[] args) {
+
+
+        int random[]={6,-4,12,0,-10};
+        int x =12;
+        int y = Arrays.binarySearch(random,x);
+        System.out.println(y);
+
+    }
+}
