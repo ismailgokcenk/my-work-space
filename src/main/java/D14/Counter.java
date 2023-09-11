e1abe93fab9a395a374cf4133c30c089a3ccb51b
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2023-09-12 00:18:57 +0200

    d1403

---
+++ src/main/java/D14/Counter.java
@@ -0,0 +1,33 @@
+package D14;
+
+public class Counter {
+
+    int count;
+    static int stCount;
+    public Counter(){
+        count ++;
+        stCount++;
+    }
+
+    public int getCount(){
+        return count;
+    }
+    public static int getStCount(){
+        return stCount;
+    }
+
+    public static void main(String[] args) {
+
+
+        Counter cs1 = new Counter();
+        //Counter cs2 = new Counter();
+        Counter cs3 = new Counter();
+        Counter cs4 = new Counter();
+        Counter cs5 = new Counter();
+        Counter cs6 = new Counter();
+
+        System.out.println("caunt is = " + cs6.getCount());
+        System.out.println("StCount is = " + cs6.getStCount());
+    }
+
+}
---
+++ src/main/java/D14/Deneme3.java
@@ -0,0 +1,20 @@
+package D14;
+
+public class Deneme3 {
+
+    public static void main(String[] args) {
+
+        add();
+
+    }
+
+    public static void add(){
+
+    }
+
+    public void concat(){
+        add();
+    }
+
+
+}
