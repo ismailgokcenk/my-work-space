e7c145dab0c56f3c9f7e2c8cf0fe3dc429a87fab
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2023-09-13 23:57:36 +0200

    d1502

---
+++ src/main/java/D15/passByValuePassBByReference.java
@@ -0,0 +1,27 @@
+package D15;
+
+public class passByValuePassBByReference {
+    public static void main(String[] args) {
+
+     int fiyat = 100;
+        System.out.println("%10 indirimli fiyat  :  "  +indirim10(fiyat));
+        System.out.println("%20 indirimli fiyat  :  "  +indirim20(fiyat));
+        System.out.println("%25 indirimli fiyat  :  "  +indirim25(fiyat));
+
+    }
+    public static int indirim10(int fiyat){
+        fiyat *= 0.90;
+        return fiyat;
+
+    }
+    public static int indirim20(int fiyat){
+        fiyat *=0.80;
+        return fiyat;
+    }
+    public static int indirim25(int fiyat){
+        fiyat *=0.75;
+        return fiyat;
+    }
+
+
+}
