64974e3cbde2bcf182c6bbd85c3ed7be4a9787c6
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2023-09-18 23:30:41 +0200

    d2001

---
+++ src/main/java/D20/Exception01.java
@@ -0,0 +1,27 @@
+package D20;
+
+
+
+public class Exception01 {
+
+    public static void main(String[] args) {
+
+ Bolme(10,0);
+
+
+
+
+    }
+
+    private static int  Bolme(int x, int y ) {
+
+        System.out.println(x/y);
+
+        return x;
+
+
+
+    }
+
+
+}
