05c345e5294c9319e2c3dcbc69b7d582e176cc34
İsmail Gökçen Kılıç <ismailgokcenk@gmail.com>
2023-10-12 15:37:50 +0200

    d3102

--- src/main/java/D32/SQLd05.java
+++ src/main/java/D32/SQLd05.java
@@ -1,207 +1,129 @@
-package D32;
+--------------DAY'7 NT---------------------
+        --29-GROUP BY clause:
+
+        CREATE TABLE manav
+        (
+        isim varchar(50),
+        urun_adi varchar(50),
+        urun_miktar int
+        );
+
+        INSERT INTO manav VALUES( 'Ali', 'Elma', 5);
+        INSERT INTO manav VALUES( 'Ayse', 'Armut', 3);
+        INSERT INTO manav VALUES( 'Veli', 'Elma', 2);
+        INSERT INTO manav VALUES( 'Hasan', 'Uzum', 4);
+        INSERT INTO manav VALUES( 'Ali', 'Armut', 2);
+        INSERT INTO manav VALUES( 'Ayse', 'Elma', 3);
+        INSERT INTO manav VALUES( 'Veli', 'Uzum', 5);
+        INSERT INTO manav VALUES( 'Ali', 'Armut', 2);
+        INSERT INTO manav VALUES( 'Veli', 'Elma', 3);
+        INSERT INTO manav VALUES( 'Ayse', 'Uzum', 2);
 
-public class SQLd05 {
 
+        SELECT * FROM manav;
 
---------------DAY'4 NT-------------------
-            --23-ALIASES  : Etiket/Rumuz : tablo veya sütunlara gecici isimler verebiliriz.
+        --Manav tablosundaki tüm isimleri ve her bir isim için toplam ürün miktarını görüntüleyiniz.
+
+        SELECT isim,SUM(urun_miktar)
+        FROM manav
+        GROUP BY isim
 
-    CREATE TABLE workers(
-            calisan_id char(9),
-    calisan_isim varchar(50),
-    calisan_dogdugu_sehir varchar(50)
-);
+        --Manav tablosundaki tüm isimleri ve her bir isim için toplam ürün miktarını,
+        --toplam ürün miktarına göre AZALAN sıralayınız.
 
-    INSERT INTO workers VALUES(123456789, 'Ali Can', 'Istanbul');
-    INSERT INTO workers VALUES(234567890, 'Veli Cem', 'Ankara');
-    INSERT INTO workers VALUES(345678901, 'Mine Bulut', 'Izmir');
+        SELECT isim,SUM(urun_miktar) toplam_kg
+        FROM manav
+        GROUP BY isim
+        ORDER BY SUM(urun_miktar) DESC
 
-    SELECT * FROM workers;
+        --Her bir ismin aldığı her bir ürünün toplam miktarını isme göre sıralı görüntüleyiniz.
+        SELECT isim,urun_adi,SUM(urun_miktar)
+        FROM manav
+        GROUP BY isim,urun_adi
+        ORDER BY isim
 
-    SELECT calisan_id AS id  FROM workers;  --sütunun ismi degismedi sadece id olarak goruntuledik
-    SELECT calisan_id FROM workers;  --bu da aynı sonucu verir
+        --NOT:group by ile gruplama yapıldığında SELECTden sonra sadece gruplanan sütun adı veya
+        --herhangi bir sütunun AGGREGATE fonk. ile kullanımı mümkündür.
 
-    SELECT calisan_id || ' - ' || calisan_isim AS id_and_isim FROM workers;  -- || ile biriki sütunu birleştiriyoruz
-																			--  ' - ' || ile bosluk koyuyoruz
+        SELECT isim,urun_adi,SUM(urun_miktar)
+        FROM manav
+        GROUP BY isim--hata verir
 
-    SELECT * FROM workers AS w ; -- best practis kısaltmada ilk harf. aynı harfle baska tabloalr varsa bir kaç harf ile kısaltılır
+        --NOT : GROUP BY dan sonraki sütun sırası önemli, ilk sütundan başlayarak gruplar, AGGREGATE fonk. kullanıldığında
+        --en son (en içteki) gruba göre hesaplanır.
 
+        --ürün ismine göre her bir ürünü alan toplam kişi sayısı gösteriniz.
+        SELECT urun_adi,COUNT(DISTINCT isim) kisi_sayisi
+        FROM manav
+        GROUP BY urun_adi
 
+        --Isme göre alınan toplam ürün miktarı ve ürün çeşit miktarını bulunuz
+        SELECT isim,SUM(urun_miktar) toplam_kg,COUNT(DISTINCT urun_adi) urun_cesidi
+        FROM manav
+        GROUP BY isim
 
+        --Alinan ürün miktarina gore musteri sayisinı ,
+        --musteri sayisina göre artan sıralayınız.
 
---24-SUBQUERY--NESTED QUERY
+        SELECT urun_miktar, COUNT(DISTINCT isim) musteri_sayisi
+        FROM manav
+        GROUP BY urun_miktar
+        ORDER BY musteri_sayisi
 
---24-a-SUBQUERY: WHERE ile kullanımı
 
-    SELECT *FROM calisanlar3; -- onceki derslte olusturdugumuz tablo 3.günde
-    SELECT * FROM markalar ;  -- onceki derslte olusturdugumuz tablo 3.günde
+        --30-HAVING clause:group by komutundan sonra filtrelemek için aggregate fonk ile birlikte kullanılır
 
---marka_id si 100 olan markada çalışanları listeleyiniz.
-            --1.yol)
-    SELECT marka_isim FROM markalar WHERE marka_id=100; --Vakko
+        DROP TABLE personel;
 
-    SELECT * FROM calisanlar3 WHERE isyeri='Vakko';
+        CREATE TABLE personel  (
+        id int,
+        isim varchar(50),
+        sehir varchar(50),
+        maas int,
+        sirket varchar(20)
+        );
 
+        INSERT INTO personel VALUES(123456789, 'Ali Yilmaz', 'Istanbul', 5500, 'Honda');
+        INSERT INTO personel VALUES(234567890, 'Veli Sahin', 'Istanbul', 4500, 'Toyota');
+        INSERT INTO personel VALUES(345678901, 'Mehmet Ozturk', 'Ankara', 3500, 'Honda');
+        INSERT INTO personel VALUES(456789012, 'Mehmet Ozturk', 'Izmir', 6000, 'Ford');
+        INSERT INTO personel VALUES(567890123, 'Mehmet Ozturk', 'Ankara', 7000, 'Tofas');
+        INSERT INTO personel VALUES(678901245, 'Veli Sahin', 'Ankara', 4500, 'Ford');
+        INSERT INTO personel VALUES(123456710, 'Hatice Sahin', 'Bursa', 4500, 'Honda');
 
---2.yol)  bu yol daha dinamik
-    SELECT * FROM calisanlar3 WHERE isyeri = (SELECT marka_isim FROM markalar WHERE marka_id=100);  --parantez ici ile daha dinamik yaptık
-		--OUTERQUERY							-- BURASI ALT SORGU --> SUBQUERY
+        SELECT * FROM personel;
 
+        --Her bir şirketin MIN maas bilgisini, bu bilgi eğer 4000 den fazla ise görüntüleyiniz.
 
---INTERVIEW QUESTION:calisanlar3 tablosunda max maaşı alan çalışanın tüm fieldlarını listeleyiniz.
-            SELECT * FROM calisanlar3  WHERE maas = (SELECT MAX(maas) FROM calisanlar3);
+        SELECT sirket,MIN(maas) min_maas
+        FROM personel
+        GROUP BY sirket
+        HAVING MIN(maas)>4000
 
-    SELECT MAX(maas) FROM calisanlar3; -- bu max maası veriyor
+        --Maaşı 4000 den fazla olan çalışanlardan, her bir şirketin MIN maas bilgisini görüntüleyiniz.
+        SELECT sirket, MIN(maas) min_maas
+        FROM personel
+        WHERE maas>4000
+        GROUP BY sirket
 
 
---Interview Question:calisanlar3 tablosunda ikinci en yüksek maaşı gösteriniz.ÖDEV
+        --NOT:gruplama yaptıktan sonra sonuçları filtrelemek için HAVING aggregate fonk ile
+        --gruplamadan önce mevcut kayıtları filtrelemek için ise WHERE kullanılır.
 
---SELECT * FROM calisanlar3  WHERE maas = (SELECT  MAX(maas) FROM calisanlar3);  ip ucu :7000 hariç
+        --Her bir ismin aldığı toplam gelir 10000 liradan fazla ise ismi ve toplam maasi gösteren sorgu yaziniz.
 
---calisanlar3 tablosunda max veya min maaşı alan çalışanların
--- tüm fieldlarını gösteriniz.
+        SELECT isim, SUM(maas) toplam_gelir
+        FROM personel
+        GROUP BY isim
+        HAVING SUM(maas)>10000
 
-            SELECT * FROM calisanlar3  WHERE maas = (SELECT MAX(maas) FROM calisanlar3) OR maas= (SELECT MIN(maas) FROM calisanlar3);
+        --Eğer bir şehirde çalışan personel(id) sayısı 1’den çoksa sehir ismini ve personel sayısını veren sorgu yazınız
 
---bunun birde IN ile cozumu var
+        SELECT sehir, COUNT(DISTINCT id) AS personel_sayisi
+        FROM personel
+        GROUP BY sehir
+        HAVING COUNT(DISTINCT id)>1
 
--- Ankara'da calisani olan markalarin marka id'lerini ve calisan sayilarini listeleyiniz.
+        --Eğer bir şehirde alınan MAX maas 5000’den düşükse sehir ismini ve MAX maasi veren sorgu yazınız.ÖDEV
 
-            SELECT marka_id,calisan_sayisi
-    FROM markalar
-    WHERE marka_isim IN('Pierre Cardin', 'Vakko', 'Adidas'  );
-
---2.yol  -- dinamik olan bu
-    SELECT marka_id,calisan_sayisi
-    FROM markalar
-    WHERE marka_isim IN(SELECT isyeri FROM calisanlar3 WHERE sehir='Ankara' );
-
-
---marka_id'si 101’den büyük olan marka çalişanlarinin isim, maaş ve şehirlerini listeleyiniz.
-
-
-    SELECT isim,maas,sehir
-    FROM calisanlar3
-    WHERE isyeri IN (SELECT marka_isim FROM markalar WHERE marka_id>101)
-
--- Çalisan sayisi 15.000’den cok olan markalarin isimlerini ve
---bu markada calisanlarin isimlerini ve maaşlarini listeleyiniz.
-            SELECT isim,maas,isyeri
-    FROM calisanlar3
-    WHERE isyeri IN (SELECT marka_isim FROM markalar WHERE calisan_sayisi>15000)
-
-
-
-
-
-
-
-
-
---24-b-SUBQUERY: SELECT komutundan sonra kullanımı
-
--- Her markanin id'sini, ismini ve toplam kaç şehirde bulunduğunu listeleyen bir SORGU yaziniz.
-
-
-    SELECT marka_id, marka_isim, (SELECT COUNT(DISTINCT(sehir))
-    FROM calisanlar3
-    WHERE isyeri=marka_isim) AS sehir_sayisi
-    FROM markalar--correlated subquery
-
---SELECT DISTINCT(sehir) FROM calisanlar3;--sadece farklı olanları listeler
-
---view olusturma (goruntu olusturma)
-
-    CREATE VIEW marka_sehir_sayisi AS
-    SELECT marka_id, marka_isim, (SELECT COUNT(DISTINCT(sehir))
-    FROM calisanlar3
-    WHERE isyeri=marka_isim) AS sehir_sayisi
-    FROM markalar
-
--- Her markanin ismini, calisan sayisini ve o markaya ait calisanlarin maksimum maaşini listeleyen bir Sorgu yaziniz.
-
-    SELECT marka_isim,calisan_sayisi,(SELECT MAX(maas) FROM calisanlar3 WHERE isyeri=marka_isim) max_maas   -- bu max_maas gecici tablo ismi
-    FROM markalar;
-
--- Her markanin ismini, calisan sayisini ve o markaya ait calisanlarin maksimum ve min maaşini listeleyen bir Sorgu yaziniz.
-
-    SELECT marka_isim,calisan_sayisi,(SELECT MAX(maas) FROM calisanlar3 WHERE isyeri=marka_isim) max_maas,
-            (SELECT MIN(maas) FROM calisanlar3 WHERE isyeri=marka_isim) min_maas   -- bu max_maas gecici tablo ismi
-    FROM markalar;  --burada sutunları sıralıyoruz konus yazmıyoruz o yuzden AND ile baglayamayız
-
-
-
---25-EXISTS Cond.
-
-            --Bir SQL sorgusunda alt sorgunun sonucunun boş olup olmadığını kontrol etmek için kullanılır.
---Eğer alt sorgu sonucu boş değilse, koşul sağlanmış sayılır ve sorgunun geri kalanı işletilir.
---Alt sorgu en az bir satır döndürürse sonucu EXISTS doğrudur.
-            --Alt sorgunun satır döndürmemesi durumunda, sonuç EXISTS yanlıştır.
-
-
-    CREATE TABLE mart
-            (
-                    urun_id int,
-                    musteri_isim varchar(50),
-    urun_isim varchar(50)
-);
-
-    INSERT INTO mart VALUES (10, 'Mark', 'Honda');
-    INSERT INTO mart VALUES (20, 'John', 'Toyota');
-    INSERT INTO mart VALUES (30, 'Amy', 'Ford');
-    INSERT INTO mart VALUES (20, 'Mark', 'Toyota');
-    INSERT INTO mart VALUES (10, 'Adam', 'Honda');
-    INSERT INTO mart VALUES (40, 'John', 'Hyundai');
-    INSERT INTO mart VALUES (20, 'Eddie', 'Toyota');
-
-    CREATE TABLE nisan
-            (
-                    urun_id int ,
-                    musteri_isim varchar(50),
-    urun_isim varchar(50)
-);
-
-    INSERT INTO nisan VALUES (10, 'Hasan', 'Honda');
-    INSERT INTO nisan VALUES (10, 'Kemal', 'Honda');
-    INSERT INTO nisan VALUES (20, 'Ayse', 'Toyota');
-    INSERT INTO nisan VALUES (50, 'Yasar', 'Volvo');
-    INSERT INTO nisan VALUES (20, 'Mine', 'Toyota');
-
-    select * from mart;
-    select * from nisan;
-
---Mart ayında 'Toyota' satışı yapılmış ise Nisan ayındaki tüm ürünlerin bilgilerini listeleyiniz.
-
-            SELECT *
-    FROM nisan
-    WHERE EXISTS (SELECT urun_isim FROM mart WHERE urun_isim='Toyota')
-
---Mart ayında 'Volvo' satışı yapılmış ise Nisan ayındaki tüm ürünlerin bilgilerini listeleyiniz.
-            SELECT *
-    FROM nisan
-    WHERE EXISTS (SELECT urun_isim FROM mart WHERE urun_isim='Volvo')
-
-
-
---Mart ayında satılan ürünün urun_id ve musteri_isim'lerini, eğer Nisan ayında da satılmışsa, listeleyen bir sorgu yazınız.
-
-    SELECT urun_id,musteri_isim
-    FROM mart
-    WHERE EXISTS (SELECT urun_isim FROM nisan WHERE nisan.urun_isim=mart.urun_isim);
-
---tablolar için alias kullanalım
-    SELECT urun_id,musteri_isim
-    FROM mart AS m
-    WHERE EXISTS (SELECT urun_isim FROM nisan AS n WHERE n.urun_isim=m.urun_isim);
-
-
-
-
-
-
-
-
-
-
-
-}
+        --31-UNION:iki farklı sorgu sonucunu birleştirerek görüntülemeyi sağlar
